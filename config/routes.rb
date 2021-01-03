27580b45e25dabe6dfb31487751134cc6ae3b163
Julien Bouland <bouland.julien@gmail.com>
2021-01-03 17:10:03 +0100

    Subcritpions: controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount StripeEvent::Engine, at: '/stripe-webhooks'
 
   devise_for :users
+
+  get '/plans' => 'pages#plans'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
