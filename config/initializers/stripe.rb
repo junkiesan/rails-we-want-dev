c1dcae4d0c023ee605f0728dd1cc3587cf733009
Julien Bouland <bouland.julien@gmail.com>
2021-01-05 14:17:43 +0100

    strip fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,12 +4,28 @@ Rails.configuration.stripe = {
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
  }
 
-#  StripeEvent.configure.do |events|
-#   events.subscribe 'charge.failed' do |event|
-#     event.class
-#     event.type
-#     event.data.object
-#   end
-#  end
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
+ Stripe.api_key = Rails.configuration.stripe[:secret_key]
+ StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
+
+ StripeEvent.configure do |events|
+  events.subscribe 'charge.failed' do |event|
+    event.class       #=> Stripe::Event
+    event.type        #=> "charge.failed"
+    event.data.object #=> #<Stripe::Charge:0x3fcb34c115f8>
+  end
+
+  events.subscribe 'charge.succeeded' do |event|
+    event.class       #=> Stripe::Event
+    event.type        #=> "charge.succeeded"
+    data = event.data.object
+
+    puts "Amount: #{data.amount}"
+    puts "Description: #{data.description}"
+
+    if data.paid
+      # extend account access
+      
+    end
+  end
+ end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount StripeEvent::Engine, at: '/stripe-webhooks'
+  mount StripeEvent::Engine, at: '/webhooks/stripe'
 
   devise_for :users
 
